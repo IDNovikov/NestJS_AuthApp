dd33dda51cb76bebd2869f8f7afaba77546d744e
Ilya <affective.neurosis@gmail.com>
2026-01-30 11:54:04 +0300

    FIX: globalExcFilter

--- src/modules/core/filters/globalException.filter.ts
+++ src/modules/core/filters/globalException.filter.ts
@@ -27,10 +27,9 @@ export class GlobalExceptionFilter<T> implements ExceptionFilter {
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-        if(['graphql'].includes(host.getType())){
-          throw new HttpException(this._response(status, req, exception), status)
-          
-        }
+    if (['graphql', 'rmq'].includes(host.getType())) {
+      throw new HttpException(this._response(status, req, exception), status);
+    }
     res.status(status).json(this._response(status, req, exception));
   }
 
